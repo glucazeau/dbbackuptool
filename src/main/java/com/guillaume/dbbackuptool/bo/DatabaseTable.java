79e7203095d9ce1f68a5045539e080a1e91fae3e
Guillaume Lucazeau <glucazeau@gmail.com>
2015-09-25 17:14:51 +0200

    Refactoring

--- src/main/java/com/guillaume/dbbackuptool/bo/DatabaseTable.java
+++ src/main/java/com/guillaume/dbbackuptool/bo/DatabaseTable.java
@@ -1,14 +1,28 @@
 package com.guillaume.dbbackuptool.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseTable {
 
 	private String name;
+	
+	private List<TableField> fields;
 
 	public DatabaseTable(String name) {
 		this.name = name;
+		this.fields = new ArrayList<TableField>();
 	}
 	
 	public String getName() {
 		return this.name;
+	}
+	
+	public List<TableField> getFields() {
+		return this.fields;
+	}
+	
+	public void setFields(List<TableField> fields) {
+		this.fields = fields;
 	}
 }
