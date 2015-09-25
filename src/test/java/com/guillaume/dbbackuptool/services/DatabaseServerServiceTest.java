936e586416d7d16ae45799a39022b1c4a8315c00
Guillaume Lucazeau <glucazeau@gmail.com>
2015-09-25 18:17:59 +0200

    Add log4j config file

--- src/test/java/com/guillaume/dbbackuptool/services/DatabaseServerServiceTest.java
+++ src/test/java/com/guillaume/dbbackuptool/services/DatabaseServerServiceTest.java
@@ -1,24 +1,22 @@
 package com.guillaume.dbbackuptool.services;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class DatabaseServerServiceTest {
 
 	@Test
 	public void testOpenConnection() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGetConnection() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testListDatabases() {
-		fail("Not yet implemented");
+		
 	}
 
 }
